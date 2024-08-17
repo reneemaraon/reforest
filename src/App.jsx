e963100bd22d004afe3a04be0f8e1c1db9c88284
Francesca Renee Maraon <reneemaraon@gmail.com>
2024-08-17 22:50:01 +0800

    first commit vite scaffold

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg';
+
+function App() {
+  return (
+    <div className="">
+      <div className="nav w-full">
+        <h1 className="text-amber-600">Hi</h1>
+        <div className="w-16 h-8 bg-slate-500"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
