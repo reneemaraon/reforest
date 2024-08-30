d36892f13a467177567022b794fd81c11dddd285
Francesca Renee Maraon <reneemaraon@gmail.com>
2024-08-30 16:03:18 +0800

    initial product list

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,21 +1,22 @@
-import CustomButton from "../../common/Button";
-import ProductHighlights from "./ProductHighlights";
+import CustomButton from '../../common/Button';
+import ProductHighlights from './ProductHighlights';
 
 const Homepage = () => {
   return (
-    <div className="flex-col inline-flex gap-0 w-full">
-      <div className="bg-[#000207] relative overflow-clip w-full py-20 h-screen">
-        <img
-          className="w-[1080px] absolute left-0 bottom-0"
-          src="/src/assets/tree.png"
-        />
-        <div className="absolute bottom-1/3 gap-7 items-center right-52 origin-center inline-flex flex-col">
-          <p className="text-white font-lora text-5xl w-96 leading-[60px] text-center">
-            Let’s grow a greener world together
-          </p>
-          <div className="inline-flex gap-2.5">
-            <CustomButton> BROWSE SEEDLINGS </CustomButton>
-            <CustomButton styleSet="secondary"> LEARN MORE </CustomButton>
+    <div className="flex-col inline-flex gap-0 w-full items-center">
+      <div className="bg-[#000207] relative overflow-clip w-full inline-flex flex-col items-center py-20 h-[720px]">
+        <div className="absolute w-full top-0 max-w-[1500px] bg-red">
+          <img className="w-[1120px] object-cover" src="/src/assets/tree.png" />
+        </div>
+        <div className="w-full max-w-[1200px] py-40 px-20 h-720 relative inline-flex justify-end">
+          <div className=" gap-7 items-center inline-flex flex-col">
+            <p className="text-white font-lora text-5xl w-96 leading-[60px] text-center">
+              Let’s grow a greener world together
+            </p>
+            <div className="inline-flex gap-2.5">
+              <CustomButton> BROWSE SEEDLINGS </CustomButton>
+              <CustomButton styleSet="secondary"> LEARN MORE </CustomButton>
+            </div>
           </div>
         </div>
       </div>
