6762fbd08010e05a510d0ea6d2ef976b5760fad4
Renee Maraon <reneemaraon@gmail.com>
2024-09-05 21:42:09 +0800

    slider

---
+++ src/common/Slider.jsx
@@ -0,0 +1,31 @@
+import Icon from "./Icon";
+import { CloseIcon } from "./IconSvg";
+
+const Slider = ({ children, showModal, setShowModal }) => {
+  return (
+    <div
+      className={`${
+        showModal ? "inline-flex" : "hidden"
+      }  overflow-clip bg-black/10 bg-opacity-20 backdrop-blur-[1px] fixed top-0 right-0 left-0 z-[80] w-full md:inset-0 h-full`}
+    >
+      <div className="relative w-full h-full">
+        <div className="absolute right-0 top-0 h-full w-full max-w-[500px] bg-white shadow">
+          <button
+            type="button"
+            onClick={() => setShowModal(false)}
+            className="my-4 mx-2 absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+            data-modal-hide="popup-modal"
+          >
+            <Icon>
+              <CloseIcon />
+            </Icon>
+            <span className="sr-only">Close modal</span>
+          </button>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,13 +1,68 @@
-import CustomButton from '../../common/Button';
-import About from './About';
-import Advocacies from './Advocacies';
-import LowerBanner from './LowerBanner';
-import ProductHighlights from './ProductHighlights';
-import ShopCategories from './ShopCategories';
+import { useState } from "react";
+import CustomButton from "../../common/Button";
+import About from "./About";
+import Advocacies from "./Advocacies";
+import LowerBanner from "./LowerBanner";
+import ProductHighlights from "./ProductHighlights";
+import ShopCategories from "./ShopCategories";
+import Slider from "../../common/Slider";
 
 const Homepage = () => {
+  const [showSlider, setShowSlider] = useState(false);
+
+  const openSlider = () => setShowSlider(true);
+
+  const renderSlider = () => (
+    <Slider setShowModal={setShowSlider} showModal={showSlider}>
+      <div className="flex flex-col px-4">
+        <div className="px-4 pt-8 pb-5 border-b-1 border-opacity-50">
+          <p className="font-lora text-2xl leading-[100%]">
+            Mockup Site Disclaimer
+          </p>
+        </div>
+        <div className="w-full gap-10 flex flex-col px-4 py-9">
+          <p className="text-base decoration-primary-green">
+            This is a dummy site created for example purposes. All navigation
+            and interactive elements are non-functional and meant only for show.
+            <br />
+            <br />
+            Curious to see more? Contact me via{" "}
+            <a
+              className="underline"
+              href="mailto:reneemaraon@gmail.com"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              email
+            </a>{" "}
+            or visit my{" "}
+            <a
+              className="underline"
+              href="https://github.com/reneemaraon"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Github
+            </a>{" "}
+            for additional projects.
+          </p>
+          <div>
+            <CustomButton
+              onClick={() =>
+                window.open("https://github.com/reneemaraon", "_blank")
+              }
+            >
+              Contact Developer
+            </CustomButton>
+          </div>
+        </div>
+      </div>
+    </Slider>
+  );
+
   return (
     <div className="flex-col inline-flex gap-0 w-full items-center">
+      {renderSlider()}
       <div className="bg-[#000207] relative overflow-clip w-full inline-flex flex-col items-center py-20 h-[720px]">
         <div className="absolute w-full top-0 max-w-[1500px] bg-red">
           <img
@@ -21,8 +76,14 @@ const Homepage = () => {
               Let’s grow a greener world together
             </p>
             <div className="inline-flex gap-2.5">
-              <CustomButton> BROWSE SEEDLINGS </CustomButton>
-              <CustomButton styleSet="secondary"> LEARN MORE </CustomButton>
+              <CustomButton onClick={openSlider}>
+                {" "}
+                BROWSE SEEDLINGS{" "}
+              </CustomButton>
+              <CustomButton onClick={openSlider} styleSet="secondary">
+                {" "}
+                LEARN MORE{" "}
+              </CustomButton>
             </div>
           </div>
         </div>
