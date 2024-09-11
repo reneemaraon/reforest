25024473a943ce5cd591cec326db1f1480b89780
Renee Maraon <reneemaraon@gmail.com>
2024-09-11 16:00:45 +0800

    dynamic import for products

--- src/pages/common/ProductListItem.jsx
+++ src/pages/common/ProductListItem.jsx
@@ -1,6 +1,15 @@
 import { DiscountBadge } from "./IconSvg";
 import Icon from "./Icon";
 
+const importImage = (imageSrc) => {
+  try {
+    return require(`/src/assets/${imageSrc}`).default;
+  } catch (err) {
+    console.error(`Image not found: ${imageSrc}`);
+    return "";
+  }
+};
+
 const Discount = ({ discount }) => (
   <div className="relative w-full h-full">
     <div className="absolute top-4 right-6">
@@ -29,7 +38,7 @@ const ProductListItem = ({ product }) => {
       <div className="image w-full inline-flex justify-center h-52">
         <img
           className="group-hover/item:scale-105 transition-transform duration-300 ease-in-out object-contain"
-          src={`/src/assets/${imageSrc}`}
+          src={importImage(imageSrc)}
         />
       </div>
       <div className="bottom items-center gap-4 inline-flex flex-col">
