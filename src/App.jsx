a78ab4130e376e85b1fb2bc8b0a48e229d948a7a
Francesca Renee Maraon <reneemaraon@gmail.com>
2024-09-19 10:36:17 +0800

    transfer on load to app level

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./pages/Navbar";
-import Homepage from "./pages/Homepage/Homepage";
-import Footer from "./pages/common/Footer";
-import MockupDisclaimerPopup from "./pages/common/MockupPopup";
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './pages/Navbar';
+import Homepage from './pages/Homepage/Homepage';
+import Footer from './pages/common/Footer';
+import FullPageLoading from './pages/common/FullPageLoading';
+import MockupDisclaimerPopup from './pages/common/MockupPopup';
+import { useState } from 'react';
 
 function App() {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setIsImageLoaded(true); // Set image loaded to true once it's done
+  };
+
   return (
     <div className="bg-white w-full relative font-inter leading-[117%] text-text-dark flex-col inline-flex">
       <MockupDisclaimerPopup />
-      <Navbar />
+      {!isImageLoaded && <FullPageLoading />}
+      {isImageLoaded && <Navbar />}
       <Routes>
-        <Route index element={<Homepage />} />
+        <Route
+          index
+          element={
+            <Homepage
+              isImageLoaded={isImageLoaded}
+              handleImageLoad={handleImageLoad}
+            />
+          }
+        />
       </Routes>
       <Footer />
     </div>
--- src/pages/common/FullPageLoading.jsx
+++ src/pages/common/FullPageLoading.jsx
@@ -2,7 +2,7 @@ import LoadingAnimation from './LoadingAnimation';
 
 const FullPageLoading = () => {
   return (
-    <div className="w-full h-80 min-h-screen bg-theme-negative-base inline-flex justify-center items-center">
+    <div className="w-full h-80 min-h-screen inline-flex justify-center items-center">
       <div className="w-44">
         <LoadingAnimation />
       </div>
